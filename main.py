10512f39fe55cbaf47a2057a826801977082c618
Camilo Cuartas <recursosyapps@gmail.com>
2023-03-06 16:23:08 -0500

    Modifiqué el archivo main. Agregué funcionalidades

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
 from fastapi import FastAPI 
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get('/')
+#Para cambiar el nombre de la aplicacion
+app.title = "Mi aplicación con FastAPI"
+
+#Para cambiar la version de la aplicacion
+app.version = "0.0.1"
+
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    } ,
+    {
+        'id': 2,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    }
+]
+
+
+#los tags nos permite agrupar las rutas de la aplicación
+@app.get('/', tags = ['home'])
 def message():
-    return "Hello world!"
+    return HTMLResponse('<h1>Hello world</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            return item
+    return []
+
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str):
+
+    #funcionalidad para filtrar las películas por categoría
+    return [movie for movie in movies if movie['category'] == category]
+
