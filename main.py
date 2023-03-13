1c0288eb6a7269cf90aecd650368985d8fc73eec
Camilo Cuartas <recursosyapps@gmail.com>
2023-03-13 08:47:54 -0500

    Agregué JSONResponse y LIST al main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Body, Path, Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Optional, List
 
 
 
@@ -60,40 +60,41 @@ movies = [
 def message():
     return HTMLResponse('<h1>Hello world</h1>')
 
-@app.get('/movies', tags=['movies'])
-def get_movies():
-    return movies
+@app.get('/movies', tags=['movies'], response_model=List[Movie])
+def get_movies() -> List[Movie]:
+    return JSONResponse(content=movies)
 
 #obtener las películas mediante un id
-@app.get('/movies/{id}', tags=['movies'])
-def get_movie(id: int = Path(default=1, ge=1, le=2000)):
+@app.get('/movies/{id}', tags=['movies'], response_model=Movie)
+def get_movie(id: int = Path(default=1, ge=1, le=2000)) -> Movie:
     for item in movies:
         if item["id"] == id:
-            return item
-    return []
+            return JSONResponse(content=item)
+    return JSONResponse(content=[])
 
 #obtener las películas mediante categoría
 
-@app.get('/movies/', tags=['movies'])
-def get_movies_by_category(category: str = Query(min_length=5, max_length=15)): 
+@app.get('/movies/', tags=['movies'], response_model=List[Movie])
+def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]: 
 
     #funcionalidad para filtrar las películas por categoría
-    return [item for item in movies if item['category'] == category]
-
-@app.post('/movies/', tags=['movies'])
-def create_movie(movie: Movie):
+    data = [item for item in movies if item['category'] == category]
+    return JSONResponse(content=data)
+    
+@app.post('/movies/', tags=['movies'], response_model=dict)
+def create_movie(movie: Movie) -> dict:
     movies.append(movie.dict())  #Se guarda la peli como diccionario usando dict para evitar errores
-    return movies
+    return JSONResponse(content={"message": "Se ha registrado la película"})
 
-@app.delete('/movies/{id}', tags=['movies'])
-def delete_movie(id: int):
+@app.delete('/movies/{id}', tags=['movies'], response_model=dict)
+def delete_movie(id: int) -> dict:
     for item in movies:
         if item["id"] == id:
             movies.remove(item)
-    return movies
+    return JSONResponse(content={"message": "Se ha eliminado la película"})
 
-@app.put('/movies/{id}', tags=['movies'])
-def update_movie(id: int, movie: Movie):
+@app.put('/movies/{id}', tags=['movies'], response_model=dict)
+def update_movie(id: int, movie: Movie) -> dict:
     for item in movies:
         if item["id"] == id: 
             item['title'] = movie.title
@@ -101,7 +102,7 @@ def update_movie(id: int, movie: Movie):
             item['year'] = movie.year
             item['rating'] = movie.rating
             item['category'] = movie.category
-    return movies 
+    return JSONResponse(content={"message": "Se ha modificado la película"}) 
 
 
 
